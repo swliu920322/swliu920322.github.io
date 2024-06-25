6d1c4ded531f24e771794ca7cb9be7b13f234465
liushengwei <106484779@qq.com>
2024-06-25 10:02:50 +0800

    feat: make a tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 import deno from '@astrojs/deno';
-
+import tailwind from "@astrojs/tailwind";
 export default defineConfig({
+    integrations: [tailwind()],
     output: 'server',
     adapter: deno(),
 });
