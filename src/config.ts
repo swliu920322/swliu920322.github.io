8af64873b228b74e40868bae231b6f21bdd6488b
liushengwei <1064084779@qq.com>
2024-08-19 22:06:18 +0800

    feat: fix cv

--- src/config.ts
+++ src/config.ts
@@ -1,22 +1,22 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Max | Personal Record';
-export const SITE_DESCRIPTION = "Max's Blog";
-export const GENERATE_SLUG_FROM_TITLE = true
-export const TRANSITION_API = true
+export const SITE_TITLE = 'Max | Personal Website';
+export const SITE_DESCRIPTION = 'Max\'s Blog';
+export const TRANSITION_API = true;
+export const contact = {
+  github: 'https://github.com/swliu920322',
+  linkin: 'https://www.linkedin.com/in/shengwei-liu/',
+};
 
 export const myType = {
   medical: '中医',
   civilization: '国学',
   frontend: '前端',
-  science: '科学'
-}
-export const myTypeArr = Object.entries(myType)
+  science: '科学',
+};
+export const myTypeArr = Object.entries(myType);
 export const typeReverse = myTypeArr.reduce((r, c) => {
   const [key, val] = c;
-  return {
-    ...r,
-    [val]: key
-  }
-}, {})
+  return { ...r, [val]: key };
+}, {});
